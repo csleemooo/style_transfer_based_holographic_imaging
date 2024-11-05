cd7d319a336622c9430d33b887b9565a49943342
csleemooo <cslee@kaist.ac.kr>
2024-11-05 21:54:40 +0900

    commit

--- run.sh
+++ run.sh
@@ -17,8 +17,10 @@ python train_holography_field_retrieval_disc.py --data_name MNIST --save_dir ./e
 python train_holography_field_retrieval_disc.py --data_name MNIST --save_dir ./experiments --log_dir ./logs --exp_name half_style_half_content_disc2 --holo_weight 10 --device cuda:1 --content_weight 0.0 --style_weight 10.0 --identity_weight 10 --unknown_distance 1 --batch_size 8
 
 
-# current best simulation result
+# current best result
 # 241105
 python train_holography_field_retrieval_disc.py --data_name MNIST --save_dir ./experiments --log_dir ./logs --exp_name 241104_half_style_half_content_disc --holo_weight 10 --device cuda:1 --content_weight 0.0 --style_weight 10.0 --identity_weight 10 --unknown_distance 1 --batch_size 32
 tensorboard --logdir="/mnt/mooo/CS/style transfer based holographic imaging/code/pytorch-AdaIN/logs/MNIST/241104_half_style_half_content_disc_field_retrieval" --port=9506
 python test_field_retrieval.py --device cpu
+
+python train_holography_field_retrieval_disc.py --data_name polystyrene_bead --save_dir ./experiments --log_dir ./logs --exp_name 241104_half_style_half_content_disc --holo_weight 10 --device cuda:1 --content_weight 0.0 --style_weight 10.0 --identity_weight 10 --unknown_distance 1 --batch_size 16
